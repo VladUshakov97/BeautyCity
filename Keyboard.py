4114c76e6ca24bb739c54168c9a286cfd0149268
Aferokript <alberto.cardinale@bk.ru>
2026-07-29 14:23:08 +0300

    Сделал кнопки

--- Keyboard.py
+++ Keyboard.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 from telebot.types import ReplyKeyboardMarkup
 
 
-
 def get_main_menu():
-
     main_menu = ReplyKeyboardMarkup(resize_keyboard=True)
 
     buttons = [
@@ -19,7 +17,6 @@ def get_main_menu():
 
 
 def signup_for_salon():
-
     signup = ReplyKeyboardMarkup(resize_keyboard=True)
 
     button = [
@@ -29,17 +26,68 @@ def signup_for_salon():
         'Мастер4'
         'Мастер5'
     ]
+
     signup.add(*button)
+    return signup
 
 
-def singup_for_procedure():
+def choose_master():
+    masters = ReplyKeyboardMarkup(resize_keyboard=True)
 
+    button = [
+        'Иван Васильевич'
+        'Никита Кологривый'
+        'Джейсон Стетхэм'
+        'Алина Владимировна'
+        'Стас Простофильский'
+    ]
+
+    masters.add(*button)
+    return masters
+
+def choose_procedure():
     procedure = ReplyKeyboardMarkup(resize_keyboard=True)
 
     button = [
-        'Наращивание ресниц'
+        'Наращивание ресниц 2000'
+        'Ламинирование бровей 1200'
+        'Стрижка профессиональная 2500'
+        'Стрижка бровей 500'
+        'Покраска волос 650'
     ]
 
+    procedure.add(*button)
+    return procedure
 
 
+def choose_address():
+    address = ReplyKeyboardMarkup(resize_keyboard=True)
 
+    button = [
+        'Улица Ленина 22'
+        'Улица Спортсменских 14'
+        'Строителей 56А'
+    ]
+
+    procedure.add(*button)
+    return address
+
+
+def choose_time_procedure():
+    time_procedure = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    button = [
+        '10 00'
+        '11 00'
+        '12 00'
+        '13 00'
+        '14 00'
+        '15 00'
+        '16 00'
+        '17 00'
+        '18 00'
+        '19 00'
+        '20 00'
+    ]
+    time_procedure.add(*button)
+    return time_procedure

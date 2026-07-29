f0649af72c1057e37a9654218ce275b617c3d2c6
Aferokript <alberto.cardinale@bk.ru>
2026-07-29 14:28:42 +0300

    text и reply_markup

--- handlers.py
+++ handlers.py
@@ -1,5 +1,5 @@
 import telebot
-from dotenv import load_dorenv
+from dotenv import load_dotenv
 import os
 import Keyboard
 
@@ -12,31 +12,28 @@ bot = telebot.TeleBot(telegram_token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Добро пожаловать! Здесь вы можете записаться на прием в наш салон красоты', ReplyKeyboardMarkup=Keyboard.get_main_menu)
+    bot.send_message(message.chat.id, 'Добро пожаловать! Здесь вы можете записаться на прием в наш салон красоты', reply_markup=Keyboard.get_main_menu)
 
 
-@bot.message_handler(func=lambda message: message== 'Записаться на прием')
+@bot.message_handler(func=lambda message: message.text == 'Записаться на прием')
 def signup_for_salon(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, 'Выберите салон для проведения процедуры', ReplyKeyboardMarkup=Keyboard.choose_address)
-    bot.send_message(chat.id, 'Выберите процедуру', ReplyKeyboardMarkup=Keyboard.choose_procedure)
-    bot.send_message(chat.id, 'Выберите Мастера', ReplyKeyboardMarkup=Keyboard.choose_procedure)
-    bot.send_message(chat.id, 'Выберите Время', ReplyKeyboardMarkup=Keyboard.choose_procedure)
-    
+    bot.send_message(chat_id, 'Выберите салон для проведения процедуры', reply_markup=Keyboard.choose_address)
+    bot.send_message(chat.id, 'Выберите процедуру', reply_markup=Keyboard.choose_procedure)
+    bot.send_message(chat.id, 'Выберите Мастера', reply_markup=Keyboard.choose_procedure)
+    bot.send_message(chat.id, 'Выберите Время', reply_markup=Keyboard.choose_procedure)
 
-@bot.message_handler(func=lambda message: message == 'Записаться к Мастеру')
+
+@bot.message_handler(func=lambda message: message.text == 'Записаться к Мастеру')
 def signup_for_master(message):
     chat_id = message.chat.id
-    
 
-@bot.message_handler(func=lambda message: message == 'Записаться на процедуру')
+
+@bot.message_handler(func=lambda message: message.text == 'Записаться на процедуру')
 def singup_for_procedure(message):
     chat_id = message.chat.id
-    
 
-@bot.message_handler(func=lambda message: message == 'Записаться по телефону')
+
+@bot.message_handler(func=lambda message: message.text == 'Записаться по телефону')
 def signuo_for_phone(message):
     chat_id = message.chat.id
-    
-
-

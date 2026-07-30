7a5c0a5dc158c2e79104c181abaadd37defa0ece
Aferokript <alberto.cardinale@bk.ru>
2026-07-30 12:32:37 +0300

    добавил запись в бд

--- handlers.py
+++ handlers.py
@@ -1,10 +1,13 @@
 import telebot
 from dotenv import load_dotenv
 import os
-import Keyboard
+from Keyboard import get_main_menu, choose_master, choose_address, choose_procedure, choose_time_procedure
+import psycopg2
+from database_func import connect_to_database
+from logic import write_to_database
 
 
-load_dorenv()
+load_dotenv()
 
 telegram_token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(telegram_token)
@@ -12,16 +15,21 @@ bot = telebot.TeleBot(telegram_token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Добро пожаловать! Здесь вы можете записаться на прием в наш салон красоты', reply_markup=Keyboard.get_main_menu)
+    bot.send_message(message.chat.id, 'Добро пожаловать! Здесь вы можете записаться на прием в наш салон красоты', reply_markup=get_main_menu())
 
 
 @bot.message_handler(func=lambda message: message.text == 'Записаться на прием')
 def signup_for_salon(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, 'Выберите салон для проведения процедуры', reply_markup=Keyboard.choose_address)
-    bot.send_message(chat.id, 'Выберите процедуру', reply_markup=Keyboard.choose_procedure)
-    bot.send_message(chat.id, 'Выберите Мастера', reply_markup=Keyboard.choose_procedure)
-    bot.send_message(chat.id, 'Выберите Время', reply_markup=Keyboard.choose_procedure)
+    conn, cursor = connect_to_database()
+    bot.send_message(chat_id, 'Выберите салон для проведения процедуры', reply_markup=choose_address())
+    write_to_database('salons', message.text)
+    bot.send_message(chat_id, 'Выберите процедуру', reply_markup=choose_procedure())
+    write_to_database('services', message.text)
+    bot.send_message(chat_id, 'Выберите Мастера', reply_markup=choose_master())
+    write_to_database('masters', message.text)
+    bot.send_message(chat_id, 'Выберите Время', reply_markup=choose_time_procedure())
+    write_to_database('appointments', message.text)
 
 
 @bot.message_handler(func=lambda message: message.text == 'Записаться к Мастеру')
